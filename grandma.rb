db09c42429fb6b65d3a7141115beb562b196c516
Meghan Flynn <mflynn333@gmail.com>
2019-12-12 20:00:04 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,5 +18,7 @@ def speak_to_grandma(phrase)
     return "I LOVE YOU TOO PUMPKIN!"
   else if phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
+  else 
+    return "HUH?! SPEAK UP, SONNY!"
 end
 end 
